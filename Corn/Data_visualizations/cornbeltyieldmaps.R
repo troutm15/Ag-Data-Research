c5c262e55f7a1a9c526aedd58e96bcda7cbc2539
Tyler Netherly <3710608+tydor73@users.noreply.github.com>
2017-10-27 13:58:04 -0400

    some new code

--- Corn/Data_visualizations/cornbeltyieldmaps.R
+++ Corn/Data_visualizations/cornbeltyieldmaps.R
@@ -1,11 +1,15 @@
 # load packages
 library(ggplot2)
+library(data.table)
+install.packages("maps")
+library(maps)
+getAnywhere(maps.map_data)
 
 # Get data for corn belt yields and make a map for 3 decades: 1981-1990, 1991-2000, 2001-2010
 # Corn belt is Indiana, Illinois, Ohio, Wisconsin, Minnesota, and Iowa
 
-#corn <- read.table("C:\\Users\\nethe\\Documents\\Research\\corn_data.txt", header = TRUE, sep = "\t", fill = TRUE)
-#field_corn <- subset(corn, COMMODITY_DESC == 'CORN')
+# corn <- read.table("C:\\Users\\nethe\\Documents\\Research\\corn_data.txt", header = TRUE, sep = "\t", fill = TRUE)
+# field_corn <- subset(corn, COMMODITY_DESC == 'CORN')
 cornbelt_corn <- field_corn[ field_corn$STATE_NAME == 'INDIANA' | field_corn$STATE_NAME == 'ILLINOIS' | field_corn$STATE_NAME == 'OHIO'
                             | field_corn$STATE_NAME == 'WISCONSIN' | field_corn$STATE_NAME == 'MINNESOTA' | field_corn$STATE_NAME == 'IOWA'
                              && field_corn$AGG_LEVEL_DESC == 'COUNTY', ] 
@@ -14,3 +18,11 @@ yields <- cornbelt_corn[ cornbelt_corn$SHORT_DESC == 'CORN, GRAIN - YIELD, MEASU
 yields_80s <- yields[ yields$YEAR >= '1981' & yields$YEAR <= '1990',] 
 yields_90s <- yields[ yields$YEAR >= '1991' & yields$YEAR <= '2000',] 
 yields_00s <- yields[ yields$YEAR >= '2001' & yields$YEAR <= '2010',] 
+
+# List of counties
+map.county <- map_data('county')
+
+# Average yields for 1980s 
+cornyieldmean <- tapply(as.numeric(as.character(gsub(",", ".", yields_80s$VALUE))), yields_80s$COUNTY_NAME, mean)
+
+
